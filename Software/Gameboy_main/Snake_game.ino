bffcbf555f1314e971a361829f4e66acc70be9e1
CodreanuML <102348157+CodreanuML@users.noreply.github.com>
2024-02-12 05:20:10 -0800

    Update Snake_game.ino

--- Software/Gameboy_main/Snake_game.ino
+++ Software/Gameboy_main/Snake_game.ino
@@ -339,7 +339,7 @@ void LCD_INIT(){
      digitalWrite(RST, LOW);
      digitalWrite(RST, HIGH);  
      LcdWriteCmd(0x21);  // LCD extended commands
-     LcdWriteCmd(0xB0);  // set LCD Vop (contrast)
+     LcdWriteCmd(0xB8);  // set LCD Vop (contrast)
      LcdWriteCmd(0x04);  // set temp coefficent
      LcdWriteCmd(0x14);  // LCD bias mode 1:40
      LcdWriteCmd(0x20);  // LCD basic commands
